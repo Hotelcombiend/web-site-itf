ab1cfb7cf412dab5ff1f1b372c61d121f20517b5
Hotelcombiend <71114488+Hotelcombiend@users.noreply.github.com>
2020-10-30 12:32:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,14 +7,16 @@ html>
         <h1>Joke story</h1>
         <p>นายยศนันทน์ กลัดแขก ชื่อเล่น ภู</p>
         <img src="EiGTn-zU4AE2-G3.jpg">
+        <img src="unnamed-o6g.jpg">
         <br>
         <a href="http://www.it.kmitl.ac.th">This is a link</a>
+        <a href="https://portal.it.kmitl.ac.th:4081">This is a link</a>
 
         <h2>The unordered list</h2>
         <ui>
-            <li>Google</li>
-            <li>Yahoo</li>
-            <li>Amazon</li>
+            <li>มีนาอ่ะหน้าร้อน แต่ถ้ามีเธออ้อนอ่ะน่าารัก</li>
+            <li>เธอนี้เหมือนข้าวเลยเนอะ กินเท่าไหรก็ไม่เบื่อ</li>
+            <li>หลงทางเสียเวลา หลงสบตาเสียการทรงตัว</li>
         </ui>
     
     </body>
