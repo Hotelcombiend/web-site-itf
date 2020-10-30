ef4d9bb54b5bdcdffe356b9c6377a194bf1e9cc9
Hotelcombiend <71114488+Hotelcombiend@users.noreply.github.com>
2020-10-30 12:23:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php
-echo "<h2>PHP is fun!</h2>";
-echo "Hello world!<br>";
-?>
+html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        <h1>My First Heading</h1>
+        <p>My First Paragraph</p>
+        <img src="EiGTn-zU4AE2-G3.jpg">
+        <br>
+        <a href="http://www.it.kmitl.ac.th">This is a link</a>
+
+        <h2>The unordered list</h2>
+        <ui>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazon</li>
+        </ui>
+    
+    </body>
+</html>
